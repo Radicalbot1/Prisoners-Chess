b9b33994d9cabb29b6b4ecf216c5861475cd1a9e
CalebCattron <CattronCaleb@gmail.com>
2023-10-03 01:15:09 -0500

    Fixed board sizing, added 'smallerDimension'.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { Box, Button, Container, Grid, Paper, Typography } from '@mui/material';
 import { height } from '@mui/system';
 
 function App() {
+  const smallerDimension = Math.min(window.screen.height, window.screen.width);
+
   return (
     <CssVarsProvider>
       <Paper sx={{ width: "100vw", height: "100vh" }}>
@@ -21,7 +23,7 @@ function App() {
             <Container sx={{ width: "1vw", height: "50vh" }}></Container>
             <Typography variant="h5" sx={{ width: "23vw", height: "50vh" }}>You and a friend must communicate a spot on a chess board by flipping one coin. The board is randomized, and your friend cannot see the board before the coin is flipped.</Typography>
             <Container sx={{ width: "1vw", height: "50vh" }}></Container>
-            <ChessBoard height={window.screen.height / 2} width={window.screen.width / 2}/>
+            <ChessBoard height={smallerDimension / 1.5} width={smallerDimension / 1.5}/>
             <Container sx={{ width: "1vw", height: "50vh" }}></Container>
             <Typography variant="h5" sx={{ width: "23vw", height: "50vh" }}>I am the great and mighty zote, here to be a placeholder. Counters for wins and attempts will be tracked here, along with coin "skins".</Typography>
             <Container sx={{ width: "1vw", height: "50vh" }}></Container>
